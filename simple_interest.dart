4ac5e4e3f4d79269fc728dd8b2ea89bc768afc8f
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-11-20 20:03:36 +0545

    si using named parameter

--- simple_interest.dart
+++ simple_interest.dart
@@ -2,10 +2,7 @@ void main() {
   print(si(p: 100000, time: 2, rate: 3));
 }
 
-double si({
-  required double? p,
-  required double? time,
-  required double? rate,
-}) {
-  return ((p! * time! * rate!) / 1000);
+double si({required double p, required double time,required double rate,})
+ {
+  return ((p * time * rate) / 100);
 }
